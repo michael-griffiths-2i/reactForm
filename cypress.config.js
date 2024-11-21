ec8bd504c5583cd887c4338ee2ff3256881a1a52
ThursdayGamesDB <mickg77@hotmail.com>
2024-11-21 22:35:03 +0000

    Initial Cucumber test with Gherkin

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,45 @@
 const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
-  projectId: "dwruw7",
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
+    baseUrl: "https://duckduckgo.com",
+    specPattern: "**/*.feature",
+    setupNodeEvents,
   },
 });
-
-
